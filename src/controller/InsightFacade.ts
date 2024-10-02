624bee21db4782bed81debac9e35079a4be6a029
victor <victorfengye2004@gmail.com>
2024-10-02 03:13:02 -0700

    Implement listDatasets() and some fixes to addDataset and removeDataset

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,4 +1,5 @@
 import { IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, InsightResult } from "./IInsightFacade";
+import JSZip from "jszip";
 
 /**
  * This is the main programmatic entry point for the project.
@@ -7,54 +8,57 @@ import { IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, Insig
  */
 export default class InsightFacade implements IInsightFacade {
 	private datasets = new Map<string, InsightDataset>();
+
 	public async addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
 		// TODO: Remove this once you implement the methods!
+		if (id === null || id.includes("_") || id.trim().length === 0) {
+			throw new InsightError("Invalid Dataset ID");
+		}
 
-		return new Promise((resolve, reject) => {
-			if (id === null || id.includes("_") || id.trim().length === 0) {
-				return reject(new InsightError("Invalid Dataset ID"));
-			}
+		if (this.datasets.has(id)) {
+			throw new InsightError("Dataset Already Exists");
+		}
 
-			if (this.datasets.has(id)) {
-				return reject(new InsightError("Dataset Already Exists"));
-			}
+		if (content === null || content.trim().length === 0) {
+			throw new InsightError("Invalid Content");
+		}
 
-			if (kind !== InsightDatasetKind.Sections) {
-				return reject(new InsightError("Invalid Dataset Kind"));
-			}
+		if (kind !== InsightDatasetKind.Sections) {
+			throw new InsightError("Invalid Dataset Kind");
+		}
 
-			try {
-				return resolve(["unimplemented"]);
-			} catch (err) {
-				return reject(new InsightError("Something went wrong" + err + content));
-			}
-		});
+		const zip = new JSZip();
+		const data = await zip.loadAsync(content, { base64: true });
+		const courses = data.folder("courses");
 
-		// throw new Error(
-		// 	`InsightFacadeImpl::addDataset() is unimplemented! - id=${id}; content=${content?.length}; kind=${kind}`
-		// );
+		if (!courses) {
+			throw new InsightError("No Courses Folder");
+		}
+
+		const numRows = 0;
+
+		const dataset: InsightDataset = {
+			id,
+			kind,
+			numRows,
+		};
+
+		this.datasets.set(id, dataset);
+
+		return ["Implementation unfinished"];
 	}
 
 	public async removeDataset(id: string): Promise<string> {
 		// TODO: Remove this once you implement the methods!
-		// throw new Error(`InsightFacadeImpl::removeDataset() is unimplemented! - id=${id};`);
+		if (id === null || id.includes("_") || id.trim().length === 0) {
+			throw new InsightError("Invalid Dataset ID");
+		}
 
-		return new Promise((resolve, reject) => {
-			if (id === null || id.includes("_") || id.trim().length === 0) {
-				return reject(new InsightError("Invalid Dataset ID"));
-			}
-
-			if (!this.datasets.has(id)) {
-				return reject(new InsightError("Dataset doesn't exist"));
-			}
-
-			try {
-				this.datasets.delete(id);
-				return resolve(id);
-			} catch (err) {
-				return reject(new InsightError("Something went wrong" + err));
-			}
-		});
+		if (!this.datasets.has(id)) {
+			throw new InsightError("Dataset Doesn't Exist");
+		}
+		this.datasets.delete(id);
+		return id;
 	}
 
 	public async performQuery(query: unknown): Promise<InsightResult[]> {
@@ -64,6 +68,16 @@ export default class InsightFacade implements IInsightFacade {
 
 	public async listDatasets(): Promise<InsightDataset[]> {
 		// TODO: Remove this once you implement the methods!
-		throw new Error(`InsightFacadeImpl::listDatasets is unimplemented!`);
+		const list: InsightDataset[] = [];
+
+		this.datasets.forEach((dataset) => {
+			list.push({
+				id: dataset.id,
+				kind: dataset.kind,
+				numRows: dataset.numRows,
+			});
+		});
+
+		return list;
 	}
 }
