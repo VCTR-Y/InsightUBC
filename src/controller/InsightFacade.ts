883f9eeae9bf4761b3876fb1e6d9fdc18c8c1f97
pmruthyu <pranav.karthik10@gmail.com>
2024-10-02 23:22:12 -0700

    Add initial interfaces and types to support and validate our query

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,4 +1,4 @@
-import { IInsightFacade, InsightDataset, InsightDatasetKind, InsightResult } from "./IInsightFacade";
+import { IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, InsightResult } from "./IInsightFacade";
 
 /**
  * This is the main programmatic entry point for the project.
@@ -20,11 +20,52 @@ export default class InsightFacade implements IInsightFacade {
 
 	public async performQuery(query: unknown): Promise<InsightResult[]> {
 		// TODO: Remove this once you implement the methods!
-		throw new Error(`InsightFacadeImpl::performQuery() is unimplemented! - query=${query};`);
+		if (isQuery(query)) {
+			//console.log(query);
+		} else {
+			throw new InsightError("Invalid JSON");
+		}
+		throw new InsightError("Other error");
 	}
 
 	public async listDatasets(): Promise<InsightDataset[]> {
 		// TODO: Remove this once you implement the methods!
 		throw new Error(`InsightFacadeImpl::listDatasets is unimplemented!`);
 	}
+}
+
+interface MCOMPARATOR {
+	GT?: Record<string, number>;
+	LT?: Record<string, number>;
+	EQ?: Record<string, number>;
+}
+
+interface SCOMPARATOR {
+	IS?: Record<string, string>;
+}
+
+type FILTER = SCOMPARATOR | MCOMPARATOR | LOGICCOMPARATOR;
+
+interface LOGICCOMPARATOR {
+	AND?: FILTER[];
+	OR?: FILTER[];
+	NOT?: FILTER;
+}
+
+type WhereObject = FILTER;
+
+interface OptionsObject {
+	COLUMNS: string[];
+	ORDER?: string;
+}
+
+interface QueryObject {
+	WHERE: WhereObject;
+	OPTIONS: OptionsObject;
+}
+
+function isQuery(object: any): object is QueryObject {
+	return typeof object === "object" && object !== null;
+	// &&
+	// isWhereClause(obj.WHERE) && isOptionsClause(obj.OPTIONS);
 }
