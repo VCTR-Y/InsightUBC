3b1117534bfb7cb3cd360ad708ad235b2b13a1f6
pmruthyu <pranav.karthik10@gmail.com>
2024-11-14 17:48:32 -0800

    Initial server implementation

--- src/rest/Server.ts
+++ src/rest/Server.ts
@@ -3,6 +3,8 @@ import { StatusCodes } from "http-status-codes";
 import Log from "@ubccpsc310/folder-test/build/Log";
 import * as http from "http";
 import cors from "cors";
+import InsightFacade from "../controller/InsightFacade";
+import { InsightDatasetKind, InsightError, NotFoundError } from "../controller/IInsightFacade";
 
 export default class Server {
 	private readonly port: number;
@@ -89,11 +91,72 @@ export default class Server {
 		this.express.get("/echo/:msg", Server.echo);
 
 		// TODO: your other endpoints should go here
+		this.express.put("/dataset/:id/:kind", Server.add);
+
+		this.express.delete("/dataset/:id", Server.remove);
+
+		this.express.get("/datasets", Server.list);
+
+		this.express.post("/query", Server.query);
 	}
 
 	// The next two methods handle the echo service.
 	// These are almost certainly not the best place to put these, but are here for your reference.
 	// By updating the Server.echo function pointer above, these methods can be easily moved.
+
+	private static async query(req: Request, res: Response): Promise<void> {
+		try {
+			const result = await new InsightFacade().performQuery(req.body);
+			res.status(StatusCodes.OK).json({ result });
+		} catch (err) {
+			res.status(StatusCodes.BAD_REQUEST).json({ error: err });
+		}
+	}
+
+	private static async list(_req: Request, res: Response): Promise<void> {
+		try {
+			const result = await new InsightFacade().listDatasets();
+			res.status(StatusCodes.OK).json({ result });
+		} catch (err) {
+			res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: err });
+		}
+	}
+
+	private static async remove(req: Request, res: Response): Promise<void> {
+		const { id } = req.params;
+
+		try {
+			const result = await new InsightFacade().removeDataset(id);
+			res.status(StatusCodes.OK).json({ result });
+		} catch (err) {
+			if (err instanceof NotFoundError) {
+				res.status(StatusCodes.NOT_FOUND).json({ error: err });
+			} else if (err instanceof InsightError) {
+				res.status(StatusCodes.BAD_REQUEST).json({ error: err });
+			}
+		}
+	}
+
+	private static async add(req: Request, res: Response): Promise<void> {
+		const { id, kind } = req.params;
+		const zipContentBase64 = req.body.toString("base64");
+
+		let kindType: InsightDatasetKind = InsightDatasetKind.Sections;
+		if (kind === "sections") {
+			kindType = InsightDatasetKind.Sections;
+		} else if (kind === "rooms") {
+			kindType = InsightDatasetKind.Rooms;
+		} else {
+			res.status(StatusCodes.BAD_REQUEST).json({ error: "Invalid kind" });
+		}
+		try {
+			const result = await new InsightFacade().addDataset(id, zipContentBase64, kindType);
+			res.status(StatusCodes.OK).json({ result });
+		} catch (err) {
+			res.status(StatusCodes.BAD_REQUEST).json({ error: err });
+		}
+	}
+
 	private static echo(req: Request, res: Response): void {
 		try {
 			Log.info(`Server::echo(..) - params: ${JSON.stringify(req.params)}`);
