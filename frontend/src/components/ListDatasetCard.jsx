8e799b3c54ab18fe39002b8c7bb1f92c1594797f
pmruthyu <pranav.karthik10@gmail.com>
2024-11-21 12:51:55 -0800

    Add tooltip to show num rows for each dataset and add a grid format for the datasets

--- frontend/src/components/ListDatasetCard.jsx
+++ frontend/src/components/ListDatasetCard.jsx
@@ -6,11 +6,11 @@ function ListDatasetCard(props) {
 	return (
 		<Box className="list-dataset" m="10px" p={"5"} minH={"300px"} minW={"300px"}>
 			<Heading as="h2" size="lg" m="10px">Datasets</Heading>
-				<List>
-					{datasets.map((dataset, index) => {
-						return <DatasetCard key={dataset.id} dataset={dataset} deleteDataset={deleteDataset} selectDataset={selectDataset}></DatasetCard>;
-					})}
-				</List>
+			<Box display="grid" gridTemplateColumns="repeat(auto-fill, minmax(200px, 1fr))" gap={6} sx={{ '@media (min-width: 600px)': { gridTemplateColumns: 'repeat(2, 1fr)' } }}>
+				{datasets.map((dataset, index) => {
+					return <DatasetCard key={dataset.id} dataset={dataset} deleteDataset={deleteDataset} selectDataset={selectDataset}></DatasetCard>;
+				})}
+			</Box>
 		</Box>
 	);
 }
